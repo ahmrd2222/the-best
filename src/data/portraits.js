a69ce4f2a9129f4ba1f087e1d37e1a9ff2d8fda2
ahmrd2222 <alhalyly1020@gmail.com>
2025-06-08 17:40:52 -0600

    Update portraits.js

--- src/data/portraits.js
+++ src/data/portraits.js
@@ -1,7 +1,7 @@
 const portraitData = {
   title: "portrait Photography",
   description: "Capturing the essence and personality of each individual in beautiful settings",
-  coverImage: "https://i.ibb.co/NnJ2VdqQ/IMG-2007.jpg",
+  coverImage: "https://i.ibb.co/Tx5gNTjW/IMG-2060.jpg",
   subcategories: [
     {
       id: "nature-portraits",
