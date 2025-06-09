4546d95c1ff3bc9d4e2bda2e181aa7699f8c3f6e
ahmrd2222 <alhalyly1020@gmail.com>
2025-06-09 04:19:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-aucmmggr
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ahmrd2222/sb1-aucmmggr)
---
+++ src/data/events.js
@@ -0,0 +1,87 @@
+const eventsData = {
+  title: "Event Photography",
+  description: "Capturing special moments from life's most important celebrations",
+  coverImage: "https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  galleries: [
+    {
+      id: "ali-graduation-2025",
+      title: "Ali's Graduation – May 2025",
+      date: "May 15, 2025",
+      description: "Capturing the joy and accomplishment of Ali's university graduation ceremony",
+      coverImage: "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      photos: [
+        {
+          id: "grad-1",
+          src: "https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Graduate with diploma",
+          description: "Proudly displaying the diploma"
+        },
+        {
+          id: "grad-2",
+          src: "https://images.pexels.com/photos/7944088/pexels-photo-7944088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Graduation ceremony",
+          description: "Walking across the stage"
+        },
+        {
+          id: "grad-3",
+          src: "https://images.pexels.com/photos/6147369/pexels-photo-6147369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Graduate with family",
+          description: "Family celebration after the ceremony"
+        },
+        {
+          id: "grad-4",
+          src: "https://images.pexels.com/photos/8101622/pexels-photo-8101622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Graduates throwing caps",
+          description: "The traditional cap toss"
+        },
+        {
+          id: "grad-5",
+          src: "https://images.pexels.com/photos/6147160/pexels-photo-6147160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Graduate portrait",
+          description: "Portrait session after graduation"
+        }
+      ]
+    },
+    {
+      id: "nora-birthday-2025",
+      title: "Nora's Birthday – April 2025",
+      date: "April 10, 2025",
+      description: "Celebrating Nora's special day with friends and family",
+      coverImage: "https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      photos: [
+        {
+          id: "bday-1",
+          src: "https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Birthday celebration",
+          description: "Friends gathering for the party"
+        },
+        {
+          id: "bday-2",
+          src: "https://images.pexels.com/photos/5905865/pexels-photo-5905865.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Birthday cake",
+          description: "The birthday cake moment"
+        },
+        {
+          id: "bday-3",
+          src: "https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Opening presents",
+          description: "Unwrapping birthday gifts"
+        },
+        {
+          id: "bday-4",
+          src: "https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Birthday toast",
+          description: "Raising glasses for a toast"
+        },
+        {
+          id: "bday-5",
+          src: "https://images.pexels.com/photos/5638732/pexels-photo-5638732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Party decorations",
+          description: "Beautiful party setup and decorations"
+        }
+      ]
+    }
+  ]
+}
+
+export default eventsData
