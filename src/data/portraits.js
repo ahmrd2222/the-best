df4e9432a2ad3901a9d06f089fcd887c2c721fd0
ahmrd2222 <alhalyly1020@gmail.com>
2025-07-04 23:04:36 -0600

    Update portraits.js

--- src/data/portraits.js
+++ src/data/portraits.js
@@ -49,7 +49,7 @@ const portraitData = {
       photos: [
         {
           id: "up-1",
-          src: "https://images.pexels.com/photos/2794820/pexels-photo-2794820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          src: "https://i.ibb.co/SwB09kNm/IMG-3044.jpg",
           alt: "Woman in urban setting",
           description: "Downtown fashion portrait"
         },
