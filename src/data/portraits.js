292d14d230dc356ecb8c71390b789d94c6965ec3
ahmrd2222 <alhalyly1020@gmail.com>
2025-07-04 22:07:55 -0600

    Update portraits.js

--- src/data/portraits.js
+++ src/data/portraits.js
@@ -11,7 +11,7 @@ const portraitData = {
       photos: [
         {
           id: "np-1",
-          src: "https://ibb.co/b5Vd9mFk",
+          src: "https://i.ibb.co/b5Vd9mFk/Whats-App-Image-2025-07-04-at-9-52-47-PM.jpg",
           alt: "Woman in green forest",
           description: "Portrait session in a lush green forest"
         },
