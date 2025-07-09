e2e88beb06dd440f9068781139b00e48395b6cc1
ahmrd2222 <alhalyly1020@gmail.com>
2025-07-08 19:50:41 -0600

    Update couples.js

--- src/data/couples.js
+++ src/data/couples.js
@@ -1,48 +1,89 @@
-const couplesData = {
-  title: " commercial photography",
-  description: " Through my lens, your business looks its best, captivating customers with quality",
-  coverImage: "https://i.ibb.co/WNn8PJzB/Screenshot-20250708-191020.png",
+const eventsData = {
+  title: "Event Photography",
+  description: "Capturing special moments from life's most important celebrations",
+  coverImage: "https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   galleries: [
     {
-      id: "ahmed-lina-engagement",
-      title: "yemeni house restaurant",
-      date: "",
-      description: "",
-      coverImage: "https://i.ibb.co/JjynCMHc/IMG-2757.jpg",
+      id: "ali-graduation-2025",
+      title: "SHK BILAL Assad – May 2025",
+      date: "May 15, 2025",
+      description: " capturing best moments in the event",
+      coverImage: "https://i.ibb.co/dwbvhg9H/IMG-1638.jpg",
       photos: [
         {
-          id: "eng-1",
-          src: "https://i.ibb.co/JRTwDdfR/IMG-2650.jpg",
-          alt: "Couple by sunset",
-          description: ""
-        },
-        {
-          id: "eng-2",
-          src: "https://i.ibb.co/XZ0GVR5Y/IMG-2889.jpg",
-          alt: "Ring close-up",
-          description: ""
-        },
-        {
-          id: "eng-3",
-          src: "https://i.ibb.co/XZ0GVR5Y/IMG-2889.jpg",
-          alt: "Couple laughing",
-          description: ""
-        },
-        {
-          id: "eng-4",
-          src: "https://i.ibb.co/gZz3VmLw/IMG-2786.jpg",
-          alt: "Couple walking in garden",
+          id: "grad-1",
+          src:
+"https://i.ibb.co/k7mtBK1/IMG-1658.jpg",
+          alt: "Graduate with diploma",
           description: "."
         },
         {
-          id: "eng-5",
-          src: "https://i.ibb.co/jkrSNZFP/IMG-2880.jpg",
-          alt: "Couple silhouette",
+          id: "grad-2",
+          src: "https://i.ibb.co/GfqgY14m/IMG-1445.jpg",
+          alt: "Graduation ceremony",
           description: "."
-      }
+        },
+        {
+          id: "grad-3",
+          src: "https://i.ibb.co/Tq2qPCTn/IMG-1371.jpg",
+          alt: "Graduate with family",
+          description: "."
+        },
+        {
+          id: "grad-4",
+          src: "https://i.ibb.co/WvJKQ8sS/IMG-1539.jpg",
+          alt: "Graduates throwing caps",
+          description: "."
+        },
+        {
+          id: "grad-5",
+          src: "https://i.ibb.co/XZcYpV0b/IMG-1453.jpg",
+          alt: "Graduate portrait",
+          description: "."
+        }
+      ]
+    },
+    {
+      id: "kings University-2025",
+      title: " King's University – April 2025",
+      date: "April 10, 2025",
+      description: "Student graduation celebration",
+      coverImage:
+"https://i.ibb.co/8nbDV8wc/IMG-0473.jpg",
+      photos: [
+        {
+          id: "bday-1",
+          src: "https://i.ibb.co/VckdT4dD/IMG-0402.jpg",
+          alt: "Birthday celebration",
+          description: "."
+        },
+        {
+          id: "bday-2",
+          src: "https://i.ibb.co/kV8rzwGZ/IMG-0409-1.jpg",
+          alt: "Birthday cake",
+          description: "."
+        },
+        {
+          id: "bday-3",
+          src: "https://i.ibb.co/nscVVxZW/IMG-0346-1.jpg",
+          alt: "Opening presents",
+          description: ""
+        },
+        {
+          id: "bday-4",
+          src: "https://i.ibb.co/pvmmJCWJ/IMG-0394.jpg",
+          alt: "Birthday toast",
+          description: ""
+        },
+        {
+          id: "bday-5",
+          src: "https://i.ibb.co/S4mC37xj/IMG-0381.jpg",
+          alt: "Party decorations",
+          description: ""
+        }
       ]
     }
   ]
 }
 
-export default portraitData
+export default eventsData
