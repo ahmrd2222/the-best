9b53fd995309950ba075ff3f13f36f8ed889d0cf
ahmrd2222 <alhalyly1020@gmail.com>
2025-07-09 15:03:22 -0600

    Fix couples section with proper couples photography content

--- src/data/couples.js
+++ src/data/couples.js
@@ -1,135 +1,198 @@
-const eventsData = {
-  title: " commercial photography",
-  description: "Through my lens, your business looks its best, captivating customers with quality",
-  coverImage: "https://i.ibb.co/WNn8PJzB/Screenshot-20250708-191020.png",
+const couplesData = {
+  title: "Couples & Engagement Photography",
+  description: "Capturing love stories and intimate moments between couples in beautiful settings",
+  coverImage: "https://images.pexels.com/photos/1024993/pexels-photo-1024993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   galleries: [
     {
-      id: "hh",
-      title: "Yemeni-house restaurant",
-      date: "May 15, 2025",
-      description: "",
-      coverImage: "https://i.ibb.co/JjynCMHc/IMG-2757.jpg",
+      id: "sarah-james-engagement",
+      title: "Sarah & James - Engagement Session",
+      date: "June 15, 2024",
+      description: "A romantic engagement session in the park during golden hour",
+      coverImage: "https://images.pexels.com/photos/1024996/pexels-photo-1024996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       photos: [
         {
-          id: "grad-1",
-          src: "https://i.ibb.co/ZR76G3Gs/IMG-2650.jpg",
-          alt: "Graduate with diploma",
-          description: "."
-        },
-        {
-          id: "grad-2",
-          src: "https://i.ibb.co/YYyk4xn/IMG-2761.jpg",
-          alt: "Graduation ceremony",
-          description: "."
-        },
-        {
-          id: "grad-3",
-          src: "https://i.ibb.co/3m7ZnT0K/IMG-2778-1.jpg",
-          alt: "Graduate with family",
-          description: "."
-        },
-        {
-          id: "grad-4",
-          src: "https://i.ibb.co/BHr3kgR0/IMG-2889.jpg",
-          alt: "Graduates throwing caps",
-          description: "."
-        },
-        {
-          id: "grad-5",
-          src: "https://i.ibb.co/Lzz2pbdX/IMG-2831-1.jpg",
-          alt: "Graduate portrait",
-          description: "."
-        },
-        {
-          id: "grad-6",
-          src: "https://i.ibb.co/gb49dp48/IMG-2877.jpg",
-          alt: "Couple celebration",
-          description: ""
-        },
-        {
-          id: "grad-7",
-          src: "https://i.ibb.co/cckH4js3/IMG-2872.jpg",
-          alt: "Romantic moment",
-          description: ""
-        },
-        {
-          id: "grad-8",
-          src: "https://i.ibb.co/d4J5f2BC/IMG-2643.jpg",
-          alt: "Engagement session",
-          description: ""
-        },
-        {
-          id: "grad-9",
+          id: "eng-1",
           src: "https://images.pexels.com/photos/1024996/pexels-photo-1024996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          alt: "Couple portrait",
-          description: ""
+          alt: "Couple walking hand in hand",
+          description: "Sarah and James walking through the park"
+        },
+        {
+          id: "eng-2",
+          src: "https://images.pexels.com/photos/1024997/pexels-photo-1024997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple embracing",
+          description: "Intimate moment during golden hour"
+        },
+        {
+          id: "eng-3",
+          src: "https://images.pexels.com/photos/1024995/pexels-photo-1024995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Ring detail shot",
+          description: "Beautiful engagement ring detail"
+        },
+        {
+          id: "eng-4",
+          src: "https://images.pexels.com/photos/1024994/pexels-photo-1024994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple laughing together",
+          description: "Candid moment of joy and laughter"
+        },
+        {
+          id: "eng-5",
+          src: "https://images.pexels.com/photos/1024992/pexels-photo-1024992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Romantic kiss",
+          description: "Tender kiss in beautiful lighting"
+        },
+        {
+          id: "eng-6",
+          src: "https://images.pexels.com/photos/1024991/pexels-photo-1024991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple silhouette",
+          description: "Romantic silhouette at sunset"
+        },
+        {
+          id: "eng-7",
+          src: "https://images.pexels.com/photos/1024990/pexels-photo-1024990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple dancing",
+          description: "Dancing together in the park"
+        },
+        {
+          id: "eng-8",
+          src: "https://images.pexels.com/photos/1024989/pexels-photo-1024989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Close-up portrait",
+          description: "Intimate close-up of the couple"
+        },
+        {
+          id: "eng-9",
+          src: "https://images.pexels.com/photos/1024988/pexels-photo-1024988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple with flowers",
+          description: "Beautiful moment with spring flowers"
         }
       ]
     },
     {
-      id: "kings University-2025",
-      title: " King's University – April 2025",
-      date: "April 10, 2025",
-      description: "Student graduation celebration",
-      coverImage: "https://i.ibb.co/8nbDV8wc/IMG-0473.jpg",
+      id: "mike-anna-anniversary",
+      title: "Mike & Anna - 5th Anniversary",
+      date: "August 22, 2024",
+      description: "Celebrating five years of love with a romantic photoshoot",
+      coverImage: "https://images.pexels.com/photos/1024987/pexels-photo-1024987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       photos: [
         {
-          id: "bday-1",
-          src: "https://i.ibb.co/VckdT4dD/IMG-0402.jpg",
-          alt: "Birthday celebration",
-          description: "."
-        },
-        {
-          id: "bday-2",
-          src: "https://i.ibb.co/kV8rzwGZ/IMG-0409-1.jpg",
-          alt: "Birthday cake",
-          description: "."
-        },
-        {
-          id: "bday-3",
-          src: "https://i.ibb.co/nscVVxZW/IMG-0346-1.jpg",
-          alt: "Opening presents",
-          description: ""
-        },
-        {
-          id: "bday-4",
-          src: "https://i.ibb.co/pvmmJCWJ/IMG-0394.jpg",
-          alt: "Birthday toast",
-          description: ""
-        },
-        {
-          id: "bday-5",
-          src: "https://i.ibb.co/S4mC37xj/IMG-0381.jpg",
-          alt: "Party decorations",
-          description: ""
-        },
-        {
-          id: "bday-6",
-          src: "https://images.pexels.com/photos/1024997/pexels-photo-1024997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          alt: "Couple at event",
-          description: "Couple enjoying celebration"
-        },
-        {
-          id: "bday-7",
-          src: "https://images.pexels.com/photos/1024995/pexels-photo-1024995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          id: "ann-1",
+          src: "https://images.pexels.com/photos/1024987/pexels-photo-1024987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
           alt: "Anniversary celebration",
-          description: "Special anniversary moment"
+          description: "Mike and Anna celebrating their anniversary"
         },
         {
-          id: "bday-8",
-          src: "https://images.pexels.com/photos/1024994/pexels-photo-1024994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          alt: "Couple dancing",
-          description: "Dancing together"
+          id: "ann-2",
+          src: "https://images.pexels.com/photos/1024986/pexels-photo-1024986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Romantic dinner setup",
+          description: "Beautiful anniversary dinner setup"
         },
         {
-          id: "bday-9",
-          src: "https://images.pexels.com/photos/1024992/pexels-photo-1024992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          alt: "Romantic dinner",
-          description: "Intimate celebration"
+          id: "ann-3",
+          src: "https://images.pexels.com/photos/1024985/pexels-photo-1024985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple toasting",
+          description: "Toasting to five wonderful years"
+        },
+        {
+          id: "ann-4",
+          src: "https://images.pexels.com/photos/1024984/pexels-photo-1024984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Holding hands",
+          description: "Still holding hands after all these years"
+        },
+        {
+          id: "ann-5",
+          src: "https://images.pexels.com/photos/1024983/pexels-photo-1024983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Anniversary kiss",
+          description: "A kiss to celebrate their milestone"
+        },
+        {
+          id: "ann-6",
+          src: "https://images.pexels.com/photos/1024982/pexels-photo-1024982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Couple portrait",
+          description: "Beautiful portrait of the anniversary couple"
+        },
+        {
+          id: "ann-7",
+          src: "https://images.pexels.com/photos/1024981/pexels-photo-1024981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Walking together",
+          description: "Walking hand in hand into the future"
+        },
+        {
+          id: "ann-8",
+          src: "https://images.pexels.com/photos/1024980/pexels-photo-1024980.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Sunset couple",
+          description: "Romantic moment at sunset"
+        },
+        {
+          id: "ann-9",
+          src: "https://images.pexels.com/photos/1024979/pexels-photo-1024979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Love celebration",
+          description: "Celebrating their enduring love"
+        }
+      ]
+    },
+    {
+      id: "david-lisa-proposal",
+      title: "David & Lisa - Surprise Proposal",
+      date: "September 10, 2024",
+      description: "Capturing the magical moment of David's surprise proposal to Lisa",
+      coverImage: "https://images.pexels.com/photos/1024978/pexels-photo-1024978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      photos: [
+        {
+          id: "prop-1",
+          src: "https://images.pexels.com/photos/1024978/pexels-photo-1024978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "The proposal moment",
+          description: "The magical moment David got down on one knee"
+        },
+        {
+          id: "prop-2",
+          src: "https://images.pexels.com/photos/1024977/pexels-photo-1024977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Her reaction",
+          description: "Lisa's surprised and joyful reaction"
+        },
+        {
+          id: "prop-3",
+          src: "https://images.pexels.com/photos/1024976/pexels-photo-1024976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "The ring reveal",
+          description: "The beautiful engagement ring reveal"
+        },
+        {
+          id: "prop-4",
+          src: "https://images.pexels.com/photos/1024975/pexels-photo-1024975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "She said yes!",
+          description: "The moment she said yes!"
+        },
+        {
+          id: "prop-5",
+          src: "https://images.pexels.com/photos/1024974/pexels-photo-1024974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Celebration kiss",
+          description: "Their first kiss as an engaged couple"
+        },
+        {
+          id: "prop-6",
+          src: "https://images.pexels.com/photos/1024973/pexels-photo-1024973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Happy tears",
+          description: "Tears of joy and happiness"
+        },
+        {
+          id: "prop-7",
+          src: "https://images.pexels.com/photos/1024972/pexels-photo-1024972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Ring on finger",
+          description: "The ring perfectly placed on her finger"
+        },
+        {
+          id: "prop-8",
+          src: "https://images.pexels.com/photos/1024971/pexels-photo-1024971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Newly engaged",
+          description: "Portrait of the newly engaged couple"
+        },
+        {
+          id: "prop-9",
+          src: "https://images.pexels.com/photos/1024970/pexels-photo-1024970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Celebration embrace",
+          description: "Celebrating their engagement with a loving embrace"
         }
       ]
     }
   ]
 }
 
-export default eventsData
+export default couplesData
